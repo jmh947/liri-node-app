be04d305d3dfcd37bdb3ca7eadb939dac232c342
Jaclyn Hardy <jackihardy@yahoo.com>
2020-04-09 14:05:43 -0700

    Working concert and spotify

--- liri.js
+++ liri.js
@@ -1,79 +1,102 @@
 require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
-var Spotify = require("node-spotify-api")
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
+moment().format("MM-DD-YYYY");
 var axios = require("axios");
 var command = process.argv[2];
 var value = process.argv[3];
 
-var pick = function( command, value) { 
-    switch (command) {
-        case 'concert-this':
-            showConcertInfo(command, value,);
-            break;
-        case 'spotify-this-song':
-            showSongInfo(command, value);
-            break;
-        case 'movie-this':
-            showMovieInfo(command, value);
-            break;
-        case 'do-what-it-says':
-            showSomeInfo(command, value);
-            break;
-        default:
-            console.log("Invalid Option. Please type Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
-        }
-}
-
+var choose = function (command, value) {
+  switch (command) {
+    case "concert-this":
+      showConcertInfo(command, value);
+      break;
+    case "spotify-this-song":
+      showSongInfo(command, value);
+      break;
+    case "movie-this":
+      showMovieInfo(command, value);
+      break;
+    case "do-what-it-says":
+      showSomeInfo(command, value);
+      break;
+    default:
+      console.log(
+        "Invalid Option. Please type Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says"
+      );
+  }
+};
 
 //OMDB API key
-var omdbApi = "http://www.omdbapi.com/?i=tt3896198&apikey=936bc22f"
+var omdbApi = "http://www.omdbapi.com/?i=tt3896198&apikey=936bc22f";
 
 //Bands in town function
-function showConcertInfo( command , value) {
-    console.log(command);
-    console.log(value);
-    var artist = value || "Celine Dion"
-    var bandsInTownURL= "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function showConcertInfo(command, value) {
+  console.log(command);
+  console.log(value);
+  var artist = value || "Celine Dion";
+  var bandsInTownURL =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
 
-            axios.get(bandsInTownURL)
-            .then(function (response) {
-              // handle success
-              console.log(response.data);
-            })
-            .catch(function (error) {
-              // handle error
-              console.log(error);
-            })
-            .finally(function () {
-              // always executed
-            });
+  axios
+    .get(bandsInTownURL)
+    .then(function (response) {
 
+    for (let i = 0; i < response.data.length; i++) {
+       
+      //console.log(response.data);
+      console.log(response.data[0].venue.name);
+      console.log(response.data[0].venue.location);
+      console.log(response.data[0].datetime);
+      
+    }
+      //moment().format("MM/DD/YYYY");
+     })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 }
 
 function showSongInfo(command, value) {
-    console.log(command);
-    console.log(value);
-    var artist = value || "The Sign"
- 
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
- 
-spotify.search({ type: 'track', query: value }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
-    
+  console.log(command);
+  console.log(value);
+  var value = value || "The Sign";
+
+  spotify
+    .search({ type: "track", query: value })
+    .then(function (response) {
+      //console.log(response.tracks.items[0]);
+      console.log(response.tracks.items[0].album.name);
+      console.log(response.tracks.items[0].album.artists[0].name);
+      console.log(response.tracks.items[0].name);
+      console.log(response.tracks.items[0].preview_url);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
-var runThis = function(arg1, arg2){
-    pick(arg1, arg2)
+function showMovieInfo(command, value) {
+  console.log(command);
+  console.log(value);
+  var value = value || "Mr. Nobody";
+  console.log(
+    "If you haven't watched Mr. Nobody then you should! It is on Netflix"
+  );
+  var OMDB = "";
+  axios.get;
 }
-runThis(command, value)
+
+var runThis = function (arg1, arg2) {
+  choose(arg1, arg2);
+};
+runThis(command, value);
